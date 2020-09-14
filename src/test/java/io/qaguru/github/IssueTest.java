6243adc76ce7c03325c78d3fcf66e7d664491274
olgabest <olgapankoff@gmail.com>
2020-09-14 23:38:52 +0300

    add annotation

--- src/test/java/io/qaguru/github/IssueTest.java
+++ src/test/java/io/qaguru/github/IssueTest.java
@@ -1,24 +1,48 @@
 package io.qaguru.github;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.*;
 
+@Owner("pankovaov")
+@Feature("Работа с задачами")
 public class IssueTest {
 
-    @Test
-    public void shouldFindIssueByNumber() {
-        open("https://github.com/");
-        $(".header-search-input").click();
-        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
-        $(".header-search-input").submit();
-        $(By.linkText("eroshenkoam/allure-example"));
-        $(withText("Issues")).click();
-        $(withText("12")).should(Condition.exist);
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final String BASE_URL = "https://github.com1";
+    private static final int ISSUE = 68;
 
+    @Test
+    @DisplayName("Пользователь должен иметь возможность найти задачу по номеру")
+    public void shouldFindIssueByNumber() {
+        parameter("Репозиторий", REPOSITORY);
+        parameter("Номер задачи", ISSUE);
+        link("ГитХаб",String.format("%s/%s", BASE_URL, REPOSITORY));
+
+        step("Открываем главную страницу github", () -> {
+            open(BASE_URL);
+        });
+        step ("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+        step ("Переходим по ссылке репозитория " + REPOSITORY, () -> {
+            $(By.linkText(REPOSITORY)).click();
+        });
+        step ("Открываем страницу с задачами", () -> {
+            $(withText("Issues")).click();
+        });
+        step ("Открываем задачу с номером " + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
     }
 }
